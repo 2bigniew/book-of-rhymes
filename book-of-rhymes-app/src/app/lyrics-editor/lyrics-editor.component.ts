6237f3271d59583ccc3c996503e92106e7d93354
Zbigniew Stasiak <stasiakzb@gmail.com>
2018-05-22 23:19:16 +0200

    'lyrics-editor=component'

--- book-of-rhymes-app/src/app/lyrics-editor/lyrics-editor.component.ts
+++ book-of-rhymes-app/src/app/lyrics-editor/lyrics-editor.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LyricsEditorComponent implements OnInit {
 
+  createLyrics = false;
+  add = false;
+  lyricsDetails = false;
+  lyricsName = '';
+  lyricsOptions = [
+    { value: 'verse', rows: 16, columns: 100, lyricsName: '' },
+    { value: 'chorus', rows: 4, columns: 100, lyricsName: '' },
+    { value: 'intro', rows: 8, columns: 100, lyricsName: '' },
+    { value: 'outro', rows: 8, columns: 100, lyricsName: '' }
+  ];
+  lyricsStructure = [];
+  verseNum = 1;
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  createLyricsSection(option) {
+    const structureOption = this.lyricsOptions.filter(lyricsOption =>  lyricsOption.value === option )[0];
+    this.lyricsStructure.map( section => {
+      if (section.value === 'verse') {
+        console.log(this.verseNum)
+        this.verseNum += 1;
+      }
+    });
+    this.lyricsStructure.push(structureOption);
+  }
+
+  getLyricsName(event) {
+    this.lyricsName = event.target.value;
+    this.createLyrics = true;
+    this.lyricsOptions.map( option => option.lyricsName = this.lyricsName);
+  }
+
+  addLyrics() {
+    this.add = !this.add;
+  }
+
+  addLyricsDetails() {
+    this.lyricsDetails = !this.lyricsDetails;
   }
 
 }
